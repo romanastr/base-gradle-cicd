d8e1c791e8346667da788760c020c166131e164f
Roman Shcherbakov <roman.shcherbakov.phd@gmail.com>
2020-08-19 23:08:52 -0400

    Start enabling Kubernetes deployment for build cache nodes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+buildCache {
+    remote<HttpBuildCache> {
+        url = uri("http://localhost/cache/")
+        isPush = true
+    }
+}
+
 rootProject.name = "CICD-gradle"
 include("data-model")
 include("api")
