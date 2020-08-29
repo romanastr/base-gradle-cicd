aa065f88f2803824182944f199b8a61f09b40795
Roman Shcherbakov <63028456+romanastr@users.noreply.github.com>
2020-08-28 23:41:21 -0400

    019 use cache node (#25)
    * Use build cache node on EKS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,12 @@
+val cacheUrl = "http://" + System.getenv("BUILD_CACHE_HOST") + "/cache/"
+val isCiServer = System.getenv("CI") == "true"
+print("cacheUrl = " + cacheUrl + "\n")
+print("CI = " + isCiServer + "\n")
 buildCache {
     remote<HttpBuildCache> {
-        url = uri("http://localhost/cache/")
-        isPush = true
+        url = uri(cacheUrl)
+        isPush = isCiServer
+        isAllowInsecureProtocol = true
     }
 }
 
