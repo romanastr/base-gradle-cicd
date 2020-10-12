d4650599b4eb7217791c0a2bea8f546d0dae4d26
Roman Shcherbakov <63028456+romanastr@users.noreply.github.com>
2020-10-11 21:49:29 -0400

    036 amazon mq (#43)
    * Create Amazon MQ deployment
    
    * Add actuator healthcheck for Reporting service + correct propagation of ActiveMQ URL

--- stats-reporting/build.gradle.kts
+++ stats-reporting/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(project(":data-model"))
     implementation("org.springframework.boot", "spring-boot-starter-actuator")
+    implementation("org.springframework.boot", "spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-activemq")
     implementation("org.springframework.boot", "spring-boot-starter-jdbc")
     implementation("org.mariadb.jdbc", "mariadb-java-client", "2.6.1")
